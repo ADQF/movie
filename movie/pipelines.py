1941b2b03fa7b96fb4a6e2fa685319baf98747ee
Administrator <>
2019-01-14 12:02:28 +0800

    提交

--- movie/pipelines.py
+++ movie/pipelines.py
@@ -6,6 +6,15 @@
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 
+# 管道： 数据清洗、去重。
+# 持久化：写txt，csv。写入数据库。
+
+# scrapy框架将爬取spider模块和处理层pipeline分离开，使得程序更容易扩展。
+# spider yield生成的item会交给pipline处理。如果爬取速度跟处理速度不一致的话，scrapy框架会自动调度。
+#
+
 class MoviePipeline(object):
     def process_item(self, item, spider):
+        with open('my_meiju.txt', 'a', encoding='utf_8') as f:
+            f.write(str(item.name) + '\n')
         return item
--- movie/spiders/meiju.py
+++ movie/spiders/meiju.py
@@ -5,7 +5,22 @@ import scrapy
 class MeijuSpider(scrapy.Spider):
     name = 'meiju'      # 爬虫名。 一个项目下可能有多个爬虫，并且每个爬虫有优先级、并发等设置。scrapy crawl [spider_name]
     allowed_domains = ['meijutt.com']    # 为了防止爬虫项目自动爬取到其他网站，设置限制，每一次请求前都会检查请求的网址是否属于这个域名下，是的话才允许请求。注意：爬取日志爬取网址后响应总为None，检查allowed_domain书写是否正确。值是一级域名。
-    start_urls = ['http://www.meijutt.com/new100.html']    # 第一个请求的url，所有请求的入口
+    start_urls = ['http://www.meijutt.com/new100.html']    # 第一个请求的url，整个程序逻辑的入口。得到的response返回给 self。parse(self,response=response)
 
     def parse(self, response):
+        # print(response.status_code, response.content, response.text)
+        # 非框架下写法dom = lxml.etree.HTML(response.text) ; dom.xpath('')
+        # scrapy框架下正规写法Selector(response.text).xpath('').extract()
+
+        # 获取剧集名
+        movie_list = response.xpath('//ul[@class="top-list  fn-clear"]/li')     # [<li>对象，<li>对象]
+        #
+        for movie in movie_list:
+            # 表示在字标签基础上继续解析
+            movie.xpath('./h5/text()').extract_first()
+
+            item = MovieItem()
+            item.name = name
+            yield iltm
+
         pass
